992fe6dc7bb893161b867588ae506ddb88b9be92
EnderYurdakoc <eyurdakoc@gmail.com>
2023-03-31 23:55:11 +0300

    .env added

--- ToDoAPI/db/connect.js
+++ ToDoAPI/db/connect.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectionString = 
-    'mongodb+srv://ender:1q2w3e4r@nodejscluster.lvjmc6w.mongodb.net/NodeJsToDoApp?retryWrites=true&w=majority';
-
 
 const connectDB = (url) => {
-return mongoose.connect(connectionString, {
+return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
