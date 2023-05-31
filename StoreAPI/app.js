ade7493e661287c9f9dddb2e3e0ab4cc617d63d9
EnderYurdakoc <eyurdakoc@gmail.com>
2023-05-31 23:38:12 +0300

    initial setup

---
+++ StoreAPI/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+// async errors
+
+
+const express = require('express')
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">Products route</a>');
+})
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        // connect to DB
+        app.listen(port, console.log(`Server is listening port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
