6dd65eb9f933ef2079f26a9682900eb15d990430
Ender Y <eyurdakoc@gmail.com>
2024-01-08 23:07:52 +0300

    page, limit, skip

--- StoreAPI/controllers/products.js
+++ StoreAPI/controllers/products.js
@@ -8,7 +8,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
     const queryObject = {}
     if (featured){
         queryObject.featured = featured === 'true' ? true : false
@@ -19,7 +19,25 @@ const getAllProducts = async (req, res) => {
     if (name){
         queryObject.name = { $regex: name, $options: 'i' }
     }
-    const products = await Product.find(queryObject);
+    let result = Product.find(queryObject);
+    if (sort){
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }else{
+        result = result.sort('createdAt')
+    }
+    if (fields){
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const products = await result
     res.status(200).json({ products, nbHits: products.length })
 }
 
